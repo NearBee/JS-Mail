c465fd93002d6eb2a95e4d920a87fa028d4143e5
Nathaniel Berube <Rexar14@gmail.com>
2023-01-14 18:30:16 -0600

    Made a reply button as well as functionality

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -29,7 +29,6 @@ function send_email(event) {
   let recipients = document.querySelector("#compose-recipients").value
   let subject = document.querySelector("#compose-subject").value
   let body = document.querySelector("#compose-body").value
-  console.log(recipients, subject, body)
 
   fetch('/emails', {
     method: 'POST',
@@ -39,11 +38,14 @@ function send_email(event) {
       "body": body,
     })
   })
+
     .then((response) => response.json())
+
     .then((result) => {
-      // Print Result
-      console.log(result);
+
+      // Transition view to sent to show the email being sent
       load_mailbox('sent');
+
     });
   event.preventDefault();
 }
@@ -68,10 +70,10 @@ function load_mailbox(mailbox) {
         const emailEntries = document.createElement("div");
 
         if (email.read) {
-          emailEntries.innerHTML = `<div id="email_div_${email.id}" style="border: 1px solid #3f3e3e;border-radius: 7px; background: gray; padding: 10px 5px 10px 5px; margin: 10px 0 10px 0;">Sender: ${email.sender} Subject: ${email.subject} timestamp: ${email.timestamp}</div>`;
+          emailEntries.innerHTML = `<div id="email_div_${email.id}" class="row" style="border: 1px solid #3f3e3e;border-radius: 7px; background: #a0a0a0; padding: 10px 5px 10px 5px; margin: 10px 0 10px 0;"><strong class="col-auto">${email.sender}</strong> <div class="col-auto">${email.subject}</div> <div class="col-auto ml-auto text-muted">${email.timestamp}</div></div>`;
         }
         else {
-          emailEntries.innerHTML = `<div id="email_div_${email.id}" style="border: 1px solid #c0c1c1;border-radius: 7px; background: white; padding: 10px 5px 10px 5px; margin: 10px 0 10px 0;">Sender: ${email.sender} Subject: ${email.subject} timestamp: ${email.timestamp}</div>`;
+          emailEntries.innerHTML = `<div id="email_div_${email.id}" class="row" style="border: 1px solid #999b9b;border-radius: 7px; background: white; padding: 10px 5px 10px 5px; margin: 10px 0 10px 0;"><strong class="col-auto">${email.sender}</strong> <div class="col-auto">${email.subject}</div> <div class="col-auto ml-auto text-muted">${email.timestamp}</div></div>`;
         }
 
         document.querySelector("#emails-view").append(emailEntries);
@@ -94,7 +96,7 @@ function load_mailbox(mailbox) {
               email_view_page.innerHTML = `<br><div><strong>From:</strong> ${email.sender}</div>`;
               email_view_page.innerHTML += `<div><strong  style="padding-right: 22px;">To:</strong> ${email.recipients}</div>`;
               email_view_page.innerHTML += `<br><div><strong>Subject:</strong> ${email.subject}</div>`;
-              email_view_page.innerHTML += `<div><strong>Timestamp:</strong> ${email.timestamp}</div>`;
+              email_view_page.innerHTML += `<div style="justify-content: end;"><strong>Timestamp:</strong> ${email.timestamp}</div>`;
 
               // Archived verification
               if (!email.archived) {
@@ -103,11 +105,17 @@ function load_mailbox(mailbox) {
                 email_view_page.innerHTML += `<br><button class="btn btn-sm btn-outline-primary" id="archive">Unarchive</button>`;
               }
 
+              email_view_page.innerHTML += `<button class="btn btn-sm btn-outline-primary ml-1" id="reply">Reply</button>`;
+
               email_view_page.innerHTML += "<hr>"
               email_view_page.innerHTML += `<br><div><p>${email.body}</p></div>`;
 
               document.querySelector('#archive').addEventListener('click', () => {
                 archive_button(email.id, email.archived);
+
+              });
+              document.querySelector("#reply").addEventListener('click', () => {
+                reply_button(email.sender, email.timestamp, email.subject, email.body);
               });
             })
 
@@ -151,4 +159,29 @@ function archive_button(email_id, archive_boolean) {
         console.log('Error:', error);
       });
   }
+}
+
+function reply_button(sender, timestamp, subject, body) {
+  // Option 2: Use already made 'composeEmail' function but change fields slightly
+
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+
+  // fill coposition fields
+  document.querySelector('#compose-recipients').value = sender;
+  document.querySelector('#compose-body').value = `\n\n----\nOn ${timestamp} ${sender} wrote: \n`;
+  document.querySelector('#compose-body').value += `\n"${body}"\n----`;
+
+  // Checking for "RE: " in the subject
+  if (subject.startsWith("RE: ")) {
+    document.querySelector('#compose-subject').value = subject;
+  } else {
+    document.querySelector('#compose-subject').value = "RE: " + subject;
+  }
+
 }
